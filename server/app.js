b1f2dd5c627ee06ad6ccc76eb67640cd7ce8566f
K-Bermet <bkalbotoeva@gmail.com>
2019-10-17 10:28:54 -0500

    app.js has changed

--- server/app.js
+++ server/app.js
@@ -8,31 +8,31 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.get('/api/courses', (req, res) => {
-    res.send(courses);
+app.get('/api/people', (req, res) => {
+    res.send(people);
 });
 
-app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
+app.get('/api/people/:id', (req, res) => {
+    const course = people.find(c => c.id === parseInt(req.params.id));
     if (!course) return res.status(404).send('Course with given ID was not found!')
     res.send(course);
 });
 
-app.post('/api/courses', (req, res) => {
+app.post('/api/people', (req, res) => {
     const { error } = validateCourse(req.body); // body.error
 
     if (error) return res.status(400).send(error.details[0].message);
 
     const course = {
-        id: courses.length + 1,
+        id: people.length + 1,
         name: req.body.name
     };
-    courses.push(course);
+    people.push(course);
     res.send(course);
 });
 
-app.put('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
+app.put('/api/people/:id', (req, res) => {
+    const course = people.find(c => c.id === parseInt(req.params.id));
     if (!course) return res.status(404).send('Course with given ID was not found!');
 
     const { error } = validateCourse(req.body); // body.error
@@ -43,12 +43,12 @@ app.put('/api/courses/:id', (req, res) => {
     res.send(course);
 });
 
-app.delete('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
+app.delete('/api/people/:id', (req, res) => {
+    const course = people.find(c => c.id === parseInt(req.params.id));
     if (!course) return res.status(404).send('Course with given ID was not found!')
 
-    const index = courses.indexOf(course);
-    courses.splice(index, 1);
+    const index = people.indexOf(course);
+    people.splice(index, 1);
 
     res.send(course);
 })
@@ -66,17 +66,17 @@ function validateCourse(course) {
     return Joi.validate(course, schema);
 }
 
-const courses = [
+const people = [
     {
         id: 1,
-        name: "course1"
+        name: "Jake"
     },
     {
         id: 2,
-        name: "course2"
+        name: "Tim"
     },
     {
         id: 3,
-        name: "course3"
+        name: "Paul"
     }
 ]
